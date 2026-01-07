b4fd63157aa55eba60a8397060dc5ffeb8d266f9
Aviel Cohen <coaviel@gmail.com>
2026-01-07 12:51:23 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -15,7 +15,8 @@ import {
   X,
   Bell,
   Search,
-  ChevronDown
+  ChevronDown,
+  Mail
 } from 'lucide-react';
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
