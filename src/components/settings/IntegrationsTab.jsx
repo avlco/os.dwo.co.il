4fa8d10fdd4d1bca5f5d7be8c1023d40b99694e0
Aviel Cohen <aviel.cohen@gramss.app>
2026-01-13 10:37:49 +0200

    Remove extra newline in IntegrationsTab.jsx

--- src/components/settings/IntegrationsTab.jsx
+++ src/components/settings/IntegrationsTab.jsx
@@ -26,7 +26,6 @@ export default function IntegrationsTab() {
     };
     loadUser();
   }, []);
-  
   const [integrationSettings, setIntegrationSettings] = useState({
     googleSpreadsheetId: '',
   });
